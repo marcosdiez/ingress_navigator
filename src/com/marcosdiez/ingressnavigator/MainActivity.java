643c8c1049fab4c9226c5de7f8928cd5593d358b
Marcos Diez <marcos@unitron.com.br>
2013-12-14 12:14:06 +0000

    first post

---
+++ src/com/marcosdiez/ingressnavigator/MainActivity.java
@@ -0,0 +1,187 @@
+package com.marcosdiez.ingressnavigator;
+
+import java.util.Locale;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends FragmentActivity {
+    private static final String TAG = "IngressNavigator";
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a
+	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+	 * will keep every loaded fragment in memory. If this becomes too memory
+	 * intensive, it may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	SectionsPagerAdapter mSectionsPagerAdapter;
+
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	ViewPager mViewPager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the app.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+
+	}
+
+
+
+
+
+
+
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the sections/tabs/pages.
+	 */
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			// getItem is called to instantiate the fragment for the given page.
+			// Return a DummySectionFragment (defined as a static inner class
+			// below) with the page number as its lone argument.
+			Fragment fragment = new DummySectionFragment();
+			Bundle args = new Bundle();
+			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+			fragment.setArguments(args);
+			return fragment;
+		}
+
+		@Override
+		public int getCount() {
+			// Show 3 total pages.
+			return 7;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			Locale l = Locale.getDefault();
+			switch (position) {
+			case 0:
+				return getString(R.string.title_section1).toUpperCase(l);
+			case 1:
+				return getString(R.string.title_section2).toUpperCase(l);
+			case 2:
+				return getString(R.string.title_section3).toUpperCase(l);
+            case 3:
+                return getString(R.string.title_section4).toUpperCase(l);
+            case 4:
+                return getString(R.string.title_section5).toUpperCase(l);
+            case 5:
+                return getString(R.string.title_section6).toUpperCase(l);
+			}
+			return null;
+		}
+	}
+
+	/**
+	 * A dummy fragment representing a section of the app, but that simply
+	 * displays dummy text.
+	 */
+	public static class DummySectionFragment extends Fragment {
+		/**
+		 * The fragment argument representing the section number for this
+		 * fragment.
+		 */
+		public static final String ARG_SECTION_NUMBER = "section_number";
+
+		public DummySectionFragment() {
+		}
+
+
+        int getImageId(int id){
+            switch(id){
+                case 1:
+                    return R.drawable.p1;
+                case 2:
+                    return R.drawable.p2;
+                case 3:
+                    return R.drawable.p3;
+                case 4:
+                    return R.drawable.p4;
+                case 5:
+                    return R.drawable.p5;
+                case 6:
+                    return R.drawable.p6;
+                case 7:
+                    return R.drawable.p7;
+                default:
+                    return R.drawable.p8;
+            }
+        }
+
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
+			TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+
+            int id = getArguments().getInt(ARG_SECTION_NUMBER);
+
+			dummyTextView.setText("Portal " + id);
+
+            ImageView portalImage = (ImageView) rootView.findViewById(R.id.portal_image);
+            portalImage.setImageResource(getImageId(id));
+
+            ImageButton map_button = (ImageButton) rootView.findViewById(R.id.button_send_to_map);
+
+            map_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openGpsUrl("45","45");
+                }
+            });
+//            map_button.setImageResource(R.drawable.map);
+
+			return rootView;
+		}
+        
+        void openGpsUrl(String longitude, String latitude){
+            String theURL = "http://maps.google.com/maps?daddr=" + longitude + "," + latitude;
+            Log.d(TAG, "Opening location:" + theURL);
+            final Intent intent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(theURL));
+            startActivity(intent);
+        }
+	}
+
+}
